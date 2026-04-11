5f687a980def74c0abdf46e19126251614751090
S1lverFoot <pro31079@gmail.com>
2026-04-11 12:30:32 +0300

    Task 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
-pub fn initialize_system() {
-    println!("Hardware Controller Library: System initialized.");
+use std::time::{Duration, Instant};
+use std::thread;
+
+pub struct TelemetryCounter {
+    current_id: u64,
+}
+
+impl TelemetryCounter {
+    pub fn new(start: u64) -> Self {
+        TelemetryCounter { current_id: start }
+    }
+}
+
+impl Iterator for TelemetryCounter {
+    type Item = u64; 
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let value = self.current_id;
+        self.current_id += 1;
+
+        Some(value) 
+    }
+}
+
+pub fn process_telemetry_with_timeout<I>(iterator: I, timeout_secs: u64) 
+where
+    I: Iterator<Item = u64>,
+{
+    let timeout = Duration::from_secs(timeout_secs);
+    let start_time = Instant::now(); 
+
+    println!("--- Розпочато збір телеметрії (Таймаут: {} сек) ---", timeout_secs);
+
+    for packet_id in iterator {
+        if start_time.elapsed() >= timeout {
+            println!("--- Таймаут досягнуто! Збір даних зупинено. ---");
+        }
+        if start_time.elapsed() >= timeout {
+            println!("--- Таймаут досягнуто! Збір даних зупинено. ---");
+                break; 
+}
+        println!("Оброблено пакет телеметрії ID: {}", packet_id);
+        thread::sleep(Duration::from_millis(500));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use hardware_controller::initialize_system;
+use hardware_controller::{TelemetryCounter, process_telemetry_with_timeout};
 
 fn main() {
-    println!("Starting the Smart Helmet application...");
-    initialize_system();
+    println!("Запуск Smart Hardware Controller...\n");
+    let sensor_generator = TelemetryCounter::new(1000);
+    process_telemetry_with_timeout(sensor_generator, 3);
+    println!("\nРоботу системи успішно завершено.");
 }
